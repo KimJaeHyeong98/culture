aae63eea9c2cede24080460013ea81ea0c2c5cc1
kimjaehyeong <comslzhslzh@naver.com>
2025-07-07 10:43:49 +0900

    first commit

---
+++ src/main/java/com/culture/review/controller/SampleC.java
@@ -0,0 +1,8 @@
+package com.culture.review.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SampleC {
+    
+}
---
+++ src/main/java/com/culture/review/service/SampleService.java
@@ -0,0 +1,8 @@
+package com.culture.review.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+    
+}
