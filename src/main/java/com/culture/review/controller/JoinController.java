111501935f95245b1f10c2b189e985fea977b56f
sooyoung <sooyoungkwak9808@gmail.com>
2025-07-08 17:51:38 +0900

    [feat] join

---
+++ src/main/java/com/culture/review/controller/JoinController.java
@@ -0,0 +1,40 @@
+package com.culture.review.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import com.culture.review.vo.UserVO;
+
+
+
+
+@Controller
+public class JoinController {
+
+    @GetMapping("/join-sy")
+    public String showJoinPage(Model model) {
+        model.addAttribute("userVO", new UserVO());
+        return "join-sy";
+    }
+
+    @PostMapping("/join-sy")
+    public String joinSubmit(@Valid @ModelAttribute UserVO userVO,
+                             BindingResult bindingResult,
+                             Model model) {
+        if (bindingResult.hasErrors()) {
+            return "join-sy";
+        }
+
+        model.addAttribute("msg", userVO.getName() + "님 회원가입 성공!");
+        return "joinSuccess-sy";
+    }
+    
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/joinSuccess-sy.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%> <%@ taglib prefix="c"
+uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>가입완료</title>
+  </head>
+  <body>
+    <h1>가입완료</h1>
+    <form action="join-sy" method="post">
+      <h2>${msg}</h2>
+    </form>
+  </body>
+</html>
